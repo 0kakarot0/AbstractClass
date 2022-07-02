47d29740cbbdd86525021b6b84101c2898cbd14a
Ahtisham <ilyasahtsham@gmail.com>
2022-07-02 14:16:08 +0500

    optimize the code

--- src/main/java/animal/Farm.java
+++ src/main/java/animal/Farm.java
@@ -1,18 +1,20 @@
 package animal;
 
+import java.util.AbstractCollection;
+
 public class Farm {
 
     public static void main(String[] args) {
-        CowFoodFinder("Cow","Grass", "Moo");
+        CowFoodFinder("Cow", "Grass", "Moo");
     }
 
-    public static void CowFoodFinder(String animalName, String animalFood, String makeSound){
-        Animal animalProperties = new Cow(animalName,animalFood,makeSound);
+    public static void CowFoodFinder(String animalName, String animalFood, String makeSound) {
+        Animal animalProperties = new Cow(animalName, animalFood, makeSound);
         Cow getCow = (Cow) animalProperties;
         String getNameOfAnimal = getCow.getAnimalName();
         String getFoodOfAnimal = getCow.getAnimalFood();
         String getMakeSound = getCow.getMakeSound();
-        animalProperties.eat(getNameOfAnimal,getFoodOfAnimal);
-        System.out.println(getNameOfAnimal+" make sound " + getMakeSound);
+        animalProperties.eat(getNameOfAnimal, getFoodOfAnimal);
+        System.out.println(getNameOfAnimal + " make sound " + getMakeSound);
     }
 }
